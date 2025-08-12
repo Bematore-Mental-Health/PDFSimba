d8022cf7eb6ab60051171a654d5478132b7f06bc
Gaudencia Otara <78945952+GaudenciaOtara@users.noreply.github.com>
2025-08-12 13:31:06 +0300

    Fresh start

--- Dashboard/JS/toggle.js
+++
@@ -1,8 +0,0 @@
-  const sidebar = document.getElementById('sidebar');
-  const toggleBtn = document.getElementById('sidebarToggle');
-
-  toggleBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('show');
-    toggleBtn.classList.toggle('move-right');
-  });
-  
